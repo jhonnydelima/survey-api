51e1bb8503bd7837364072914bef8beb939ffe45
Jhonny Lima <lima.jhonny95@gmail.com>
2026-02-23 12:44:09 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,9 +13,7 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(
-      new MissingParamError("Missing param: name"),
-    );
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
   test("should return 400 if email is not provided", () => {
@@ -29,8 +27,6 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(
-      new MissingParamError("Missing param: email"),
-    );
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
 });
