925f5161b25dc615bcb8dc3360b63e420ce04e9b
Jhonny Lima <lima.jhonny95@gmail.com>
2026-02-28 19:47:29 -0300

    test: ensure CryptoAdapter returns a hash on success

--- src/infra/cryptography/crypto-adapter.spec.ts
+++ src/infra/cryptography/crypto-adapter.spec.ts
@@ -1,6 +1,12 @@
 import crypto from "node:crypto";
 import { CryptoAdapter } from "./crypto-adapter";
 
+jest.mock("node:crypto", () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve("hashed_value");
+  },
+}));
+
 describe("CryptoAdapter", () => {
   it("should call encrypt with correct value", async () => {
     const algorithm = "sha256";
@@ -8,5 +14,12 @@ describe("CryptoAdapter", () => {
     const hashSpy = jest.spyOn(crypto, "hash");
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith(algorithm, "any_value");
+  });
+
+  it("should return a hash on success", async () => {
+    const algorithm = "sha256";
+    const sut = new CryptoAdapter(algorithm);
+    const result = await sut.encrypt("any_value");
+    expect(result).toBe("hashed_value");
   });
 });
