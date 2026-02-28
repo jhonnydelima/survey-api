a0014b35c738b29fa0c0d26b17796f39ed11b639
Jhonny Lima <lima.jhonny95@gmail.com>
2026-02-28 19:50:48 -0300

    refactor(test): move sut creation to a factory method

--- src/infra/cryptography/crypto-adapter.spec.ts
+++ src/infra/cryptography/crypto-adapter.spec.ts
@@ -7,18 +7,21 @@ jest.mock("node:crypto", () => ({
   },
 }));
 
+const algorithm = "sha256";
+const makeSut = (): CryptoAdapter => {
+  return new CryptoAdapter(algorithm);
+};
+
 describe("CryptoAdapter", () => {
   it("should call encrypt with correct value", async () => {
-    const algorithm = "sha256";
-    const sut = new CryptoAdapter(algorithm);
+    const sut = makeSut();
     const hashSpy = jest.spyOn(crypto, "hash");
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith(algorithm, "any_value");
   });
 
   it("should return a hash on success", async () => {
-    const algorithm = "sha256";
-    const sut = new CryptoAdapter(algorithm);
+    const sut = makeSut();
     const result = await sut.encrypt("any_value");
     expect(result).toBe("hashed_value");
   });
