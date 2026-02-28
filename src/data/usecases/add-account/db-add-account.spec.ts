6268b6dcdb67d61a7cecff871b82b1136dd68ea9
Jhonny Lima <lima.jhonny95@gmail.com>
2026-02-28 16:58:07 -0300

    refactor(test): move encrypter to a factory method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,13 +1,22 @@
 import type { Encrypter } from "../../protocols/encrypter";
 import { DbAddAccount } from "./db-add-account";
 
-const makeSut = () => {
+const makeEncrypter = (): Encrypter => {
   class EncrypterStub implements Encrypter {
     async encrypt(value: string): Promise<string> {
       return Promise.resolve("hashed_password");
     }
   }
-  const encrypterStub = new EncrypterStub();
+  return new EncrypterStub();
+};
+
+interface SutTypes {
+  sut: DbAddAccount;
+  encrypterStub: Encrypter;
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
   const sut = new DbAddAccount(encrypterStub);
   return {
     sut,
